32c0f54afc5fdc83040cfe92773ff3737f7d3b88
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-05 10:32:33 +0900

    [Login From] use localStroage

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(e){
     e.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 };
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    paintGreeting(saveUsername);
+};
