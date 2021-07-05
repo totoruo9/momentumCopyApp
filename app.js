8782e57ffa35935e58aa5fe421c215f3311616a9
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-05 09:34:38 +0900

    js event(onclick, onchange) -> HTML input Attributes(maxlegnth, required)

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const loginButton = loginForm.querySelector("button");
 
 const innerText = document.querySelector("h1");
 
-function onLoginBtnClick(){
+/*function onLoginBtnClick(){
     console.log(loginInput.value.length);
 }
 
 function onChangeLoginInput(e){
     if(!loginInput.value.length){
         loginButton.disabled = true;
-        innerText.innerText = "Inner Text"
+        innerText.innerText = "Inner Text";
+    }else if(loginInput.value.length > 15){
+        loginButton.disabled = true;
+        innerText.innerText = "Is to long name";
     }else {
         loginButton.disabled = false;
         innerText.innerText = e.target.value;
@@ -19,4 +22,4 @@ function onChangeLoginInput(e){
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
-loginInput.addEventListener("input", onChangeLoginInput);
+loginInput.addEventListener("input", onChangeLoginInput);*/
