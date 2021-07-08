0977fc4b36640c1468f1e1ea3c27c2edf651a94c
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-08 14:16:06 +0900

    [weather]  get openweathermap API and use it.

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = 'fcfd013765e7f441f7a7474019ab924b';
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.")
